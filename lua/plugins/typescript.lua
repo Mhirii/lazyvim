ab6d307c30d78e32c61338630a3b20f056a49239
Mhirii <ahmedmhiri218@gmail.com>
2024-07-08 12:18:31 +0100

    typescript

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,3 +1,55 @@
+local decode = function(filename)
+  local file = io.open(filename, "r")
+  if not file then
+    return false
+  end
+  local content = file:read("*a")
+  file:close()
+
+  local parsed, json = pcall(vim.fn.json_decode, content)
+  if not parsed then
+    return false
+  end
+  return json
+end
+
+local function check_json_key_exists(json, ...)
+  return vim.tbl_get(json, ...) ~= nil
+end
+local check_dep = function(decoded, package_name)
+  if
+      check_json_key_exists(decoded, "dependencies", package_name)
+      or check_json_key_exists(decoded, "devDependencies", "prettier")
+  then
+    return true
+  end
+  return false
+end
+
+local get_packagejson = function()
+  local root = LazyVim.root.get()
+  local files = vim.fs.find("package.json", { path = root })
+  return files[1]
+end
+
+local has_eslint = function()
+  local package = get_packagejson()
+  if not package then
+    return false
+  end
+  local decoded = decode(package)
+  return check_dep(decoded, "eslint")
+end
+
+local has_prettier = function()
+  local package = get_packagejson()
+  if not package then
+    return false
+  end
+  local decoded = decode(package)
+  return check_dep(decoded, "prettier")
+end
+
 return {
   {
     "jellydn/typecheck.nvim",
@@ -15,4 +67,81 @@ return {
       },
     },
   },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "vuki656/package-info.nvim",
+        event = { "BufRead package.json" },
+        opts = {},
+      },
+      {
+        "David-Kunz/cmp-npm",
+        event = { "BufRead package.json" },
+        opts = {},
+      },
+    },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        { name = "npm", keyword_length = 3 },
+      }))
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "css", "html", "javascript", "jsdoc", "scss", "typescript" })
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "css-lsp", "eslint-lsp", "html-lsp", "biome" })
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    enabled = false,
+    opts = function(_, opts)
+      if has_eslint() then
+        opts.linters_by_ft = {
+          javascript = { "eslint" },
+          javascriptreact = { "eslint" },
+          typescript = { "eslint" },
+          typescriptreact = { "eslint" },
+        }
+      end
+    end,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    enabled = false,
+    opts = function(_, opts)
+      if has_prettier() then
+        opts.formatters_by_ft = {
+          javascript = { "prettier" },
+          javascriptreact = { "prettier" },
+          typescript = { "prettier" },
+          typescriptreact = { "prettier" },
+          vue = { "prettier" },
+        }
+      end
+    end,
+  },
+
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.servers.vtsls.settings.vtsls.enableMoveToFileCodeAction = false
+      opts.servers.vtsls.settings.typescript.inlayHints = false
+    end,
+  }
 }
