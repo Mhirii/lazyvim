18435fd1b5736c72109f78fd33494f9ee8ff779c
Mhirii <ahmedmhiri218@gmail.com>
2024-08-19 22:51:42 +0100

    workspace

--- lua/plugins/workspace.lua
+++ lua/plugins/workspace.lua
@@ -4,7 +4,7 @@ return {
     lazy = true,
     event = "VeryLazy",
     dependencies = {
-      "stevearc/resession.nvim",
+      -- "stevearc/resession.nvim",
     },
     config = function()
       require("workspaces").setup({
