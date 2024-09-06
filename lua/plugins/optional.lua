5c23adceeb36f250b620148cd545fa11e77dc8ae
Mhirii <ahmedmhiri218@gmail.com>
2024-09-07 00:32:03 +0100

    per hostname plugins

---
+++ lua/plugins/optional.lua
@@ -0,0 +1,44 @@
+local hostname = vim.uv.os_gethostname()
+
+local common = {
+  { import = "lazyvim.plugins.extras.coding.copilot" },
+  { import = "lazyvim.plugins.extras.coding.copilot-chat" },
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.editor.aerial" },
+  { import = "lazyvim.plugins.extras.editor.harpoon2" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.git" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.vue" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.util.octo" },
+  { import = "user.plugins.typescript" },
+  { import = "user.plugins.copilot_chat" },
+}
+
+if hostname == 'gitpod' then
+  return common
+end
+
+
+-- local machine
+return vim.list_extend(common,
+  {
+    { import = "lazyvim.plugins.extras.editor.overseer" },
+    { import = "lazyvim.plugins.extras.lang.cmake" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.nix" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
+    { import = "lazyvim.plugins.extras.lsp.neoconf" },
+    { import = "lazyvim.plugins.extras.util.dot" },
+
+    { import = "user.plugins.markdown" },
+    { import = "user.plugins.nix" },
+    { import = "user.plugins.terminal" },
+    { import = "user.plugins.typescript" },
+    { import = "user.plugins.workspace" },
+
+  }
+)
