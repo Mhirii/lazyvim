e4754834ad3e3940bd7018ae3025c75e3e9e130d
Mhirii <ahmedmhiri218@gmail.com>
2024-10-28 20:25:38 +0100

    opts

--- lua/plugins/optional.lua
+++ lua/plugins/optional.lua
@@ -1,36 +1,35 @@
 local hostname = vim.uv.os_gethostname()
 
 local common = {
-  { import = "lazyvim.plugins.extras.coding.copilot" },
-  { import = "lazyvim.plugins.extras.editor.harpoon2" },
-  { import = "lazyvim.plugins.extras.lang.docker" },
-  { import = "lazyvim.plugins.extras.lang.git" },
-  { import = "lazyvim.plugins.extras.lang.yaml" },
-  { import = "lazyvim.plugins.extras.util.octo" },
-  { import = "user.plugins.typescript" },
-  { import = "user.plugins.copilot_chat" },
+	{ import = "lazyvim.plugins.extras.coding.copilot" },
+	{ import = "lazyvim.plugins.extras.editor.harpoon2" },
+	{ import = "lazyvim.plugins.extras.lang.docker" },
+	{ import = "lazyvim.plugins.extras.lang.git" },
+	{ import = "lazyvim.plugins.extras.lang.yaml" },
+	{ import = "lazyvim.plugins.extras.util.octo" },
+	{ import = "user.plugins.typescript" },
+	{ import = "user.plugins.copilot_chat" },
 }
 
 if hostname == 'gitpod' then
-  return common
+	return common
 end
 
 
 -- local machine
 return vim.list_extend(common,
-  {
-    { import = "lazyvim.plugins.extras.editor.overseer" },
-    { import = "lazyvim.plugins.extras.lang.cmake" },
-    { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.lang.sql" },
-    { import = "lazyvim.plugins.extras.lsp.neoconf" },
-    { import = "lazyvim.plugins.extras.util.dot" },
-
-    { import = "user.plugins.markdown" },
-    { import = "user.plugins.nix" },
-    { import = "user.plugins.terminal" },
-    { import = "user.plugins.workspace" },
-
-  }
+	{
+		{ import = "lazyvim.plugins.extras.editor.overseer" },
+		{ import = "lazyvim.plugins.extras.lang.cmake" },
+		{ import = "lazyvim.plugins.extras.lang.rust" },
+		{ import = "lazyvim.plugins.extras.lang.sql" },
+		{ import = "lazyvim.plugins.extras.lsp.neoconf" },
+		{ import = "lazyvim.plugins.extras.util.dot" },
+		{ import = "user.plugins.go" },
+		{ import = "user.plugins.markdown" },
+		{ import = "user.plugins.nix" },
+		{ import = "user.plugins.supermaven" },
+		{ import = "user.plugins.terminal" },
+		{ import = "user.plugins.workspace" },
+	}
 )
