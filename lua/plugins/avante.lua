b5f3ec647704af5f89427ef5cdfb642055ec7e7a
Mhirii <ahmedmhiri218@gmail.com>
2024-11-17 02:37:16 +0100

    avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,52 +1,63 @@
 return {
-	"yetone/avante.nvim",
-	event = "VeryLazy",
-	lazy = false,
-	enabled = false,
-	version = false,
-	-- commit = "07af0a9",
-	-- pin = true,
-	dependencies = {
-		"echasnovski/mini.icons",
-		"stevearc/dressing.nvim",
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		"zbirenbaum/copilot.lua",
-		--- The below is optional, make sure to setup it properly if you have lazy=true
-		-- {
-		--   'MeanderingProgrammer/render-markdown.nvim',
-		--   opts = {
-		--     file_types = { "markdown", "Avante" },
-		--   },
-		--   ft = { "markdown", "Avante" },
-		-- },
-	},
-	opts = function(_, opts)
-		opts.provider = "copilot"
-		opts.mappings = {
-			ask = "<leader>av",
-			edit = "<leader>ae",
-			refresh = "<leader>ar",
-			--- @class AvanteConflictMappings
-			diff = {
-				ours = "co",
-				theirs = "ct",
-				both = "cb",
-				next = "]x",
-				prev = "[x",
-			},
-			jump = {
-				next = "]]",
-				prev = "[[",
-			},
-			submit = {
-				normal = "<CR>",
-				insert = "<C-s>",
-			},
-			toggle = {
-				debug = "<leader>ad",
-				hint = "<leader>ah",
-			},
-		}
-	end,
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  enabled = true,
+  version = false,
+  build = "make",
+  -- commit = "07af0a9",
+  -- pin = true,
+  dependencies = {
+    "echasnovski/mini.icons",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    {
+      "zbirenbaum/copilot.lua",
+      opts = {
+        suggestion = {
+          enabled = false,
+        }
+      }
+    },
+    --- The below is optional, make sure to setup it properly if you have lazy=true
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "Avante" },
+      },
+      ft = { "Avante" },
+    },
+  },
+  opts = {
+    provider = "copilot",
+    copilot = {
+      model = "claude-3.5-sonnet"
+    },
+    mappings = {
+      ask = "<leader>ai",
+      edit = "<leader>ae",
+      refresh = "<leader>ar",
+      --- @class AvanteConflictMappings
+      diff = {
+        ours = "co",
+        theirs = "ct",
+        both = "cb",
+        next = "]x",
+        prev = "[x",
+      },
+      jump = {
+        next = "]]",
+        prev = "[[",
+      },
+      submit = {
+        normal = "<CR>",
+        insert = "<C-CR>",
+      },
+      toggle = {
+        debug = "<leader>ad",
+        hint = "<leader>ah",
+      },
+    }
+  },
 }
